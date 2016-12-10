043c1b0b4c9fc240dcad8728e4909dd971054469
Anthony Nguyen <anknguyen@gmail.com>
2016-12-10 00:58:47 -0500

    Combine day 10 loops

--- 10/solution.py
+++ 10/solution.py
@@ -3,54 +3,46 @@
 import collections
 
 f = open("input.txt")
-raw = f.readlines()
+instructions = f.readlines()
 
 bots = collections.defaultdict(list)
 output = collections.defaultdict(list)
 
-new = []
-
-for l in raw:
-	line = l.strip()
-	parts = line.split()
-
-	if parts[0] == "value":
-		v = int(parts[1])
-		t = int(parts[5])
-
-		bots[t].append(v)
-	else:
-		new.append(line)
-
 changed = True
 while changed:
 	changed = False
-	nn = []
-	for line in new:
+	new = []
+	for line in instructions:
 		parts = line.split()
-		s = int(parts[1])
 
-		if len(bots[s]) == 2:
-			l = parts[5]
-			ln = int(parts[6])
+		if parts[0] == "value":
+			v = int(parts[1])
+			t = int(parts[5])
 
-			h = parts[10]
-			hn = int(parts[11])
-
-			if l == "output":
-				output[ln].append(min(bots[s]))
-			else:
-				bots[ln].append(min(bots[s]))
-
-			if h == "output":
-				output[hn].append(max(bots[s]))
-			else:
-				bots[hn].append(max(bots[s]))
-			changed = True
+			bots[t].append(v)
 		else:
-			nn.append(line)
+			s = int(parts[1])
+			if len(bots[s]) == 2:
+				l = parts[5]
+				ln = int(parts[6])
 
-	new = nn
+				h = parts[10]
+				hn = int(parts[11])
+
+				if l == "output":
+					output[ln].append(min(bots[s]))
+				else:
+					bots[ln].append(min(bots[s]))
+
+				if h == "output":
+					output[hn].append(max(bots[s]))
+				else:
+					bots[hn].append(max(bots[s]))
+				changed = True
+			else:
+				new.append(line)
+
+	instructions = new
 
 for b in bots:
 	if 61 in bots[b] and 17 in bots[b]:
